8c1708895d7659c9c531beb2c8ba00ef76780f28
defispartan <andrew@aave.com>
2023-12-10 02:51:21 -0600

    feat: initialize landing

---
+++ landing/src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Create Web UI</h1>
+      <p>
+        Minimal template for Vite React application with wallet connection,
+        read, and write capabilities.
+      </p>
+      <h3>Step 1: Install a package manager</h3>
+      <h3>Step 2: Run setup CLI</h3>
+      <p>npx create-web3-ui</p>
+      <p>yarn create we3-ui</p>
+      <p>pnpm create web3-ui</p>
+      <p>bunx create web3-ui</p>
+    </>
+  );
+}
+
+export default App;
