cffd1a2ff5bd6de95d4cd7304170f85db043de8d
defispartan <andrew@aave.com>
2024-01-28 16:50:04 -0600

    feat: landing layout

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { CSSProperties, useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { FiCopy, FiCheck } from "react-icons/fi";
@@ -8,7 +8,54 @@ function App() {
   const [packageManager, setPackageManager] = useState("npm");
   const [isCopied, setIsCopied] = useState(false);
 
-  const codeBlockStyle: React.CSSProperties = {
+  const boxes = [
+    {
+      title: "Template Project",
+      images: [
+        { src: "/react.png", text: "React", link: "https://react.dev" },
+        { src: "/vite.svg", text: "Vite", link: "https://vitejs.dev" },
+        {
+          src: "/jsts.png",
+          text: "TypeScript or JavaScript",
+          link: "https://typescriptlang.org",
+        },
+      ],
+    },
+    {
+      title: "EVM Developer Tooling",
+      images: [
+        { src: "/wevm.jpg", text: "Viem + Wagmi", link: "https://wevm.dev" },
+      ],
+    },
+
+    {
+      title: "Wallet Library",
+      images: [
+        {
+          src: "/connectwallet.png",
+          text: "ConnectKit",
+          link: "https://docs.family.co/connectkit/try-it-out",
+        },
+      ],
+    },
+    {
+      title: "State Management",
+      images: [
+        {
+          src: "/zustand.png",
+          text: "Zustand",
+          link: "https://github.com/pmndrs/zustand",
+        },
+        {
+          src: "/react-query.png",
+          text: "TanStack Query",
+          link: "https://tanstack.com/query",
+        },
+      ],
+    },
+  ];
+
+  const codeBlockStyle: CSSProperties = {
     position: "relative",
     width: "300px",
     height: "54px",
@@ -22,17 +69,17 @@ function App() {
     justifyContent: "space-between",
     padding: "0px",
     flex: 2,
+    background: "rgb(40, 42, 54)",
   };
 
-  const managerText = `${
+  const managerText =
     packageManager === "bun"
-      ? "bunx create web3-ui"
+      ? "bunx create-web3-ui"
       : packageManager === "pnpm"
       ? "pnpm create web3-ui"
       : packageManager === "yarn"
       ? "yarn create web3-ui"
-      : "npx create-web3-ui"
-  }`;
+      : "npx create-web3-ui";
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(managerText);
@@ -41,126 +88,124 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Create Web3 UI</h1>
-      <h3>{`Web3 Connected in Seconds`}</h3>
-      <div
-        className="manager-block"
-        style={{ textAlign: "center", width: "80%", margin: "0 auto" }}
-      >
-        <div className="manager">
-          {["bun", "pnpm", "yarn", "npm"].map((manager) => (
-            <div
-              style={{
-                display: "inline-block",
-                padding: "10px",
-                cursor: "pointer",
-                borderLeft: "1px solid slategray",
-                borderTop: "1px solid slategray",
-                borderRight: "1px solid slategray",
-                borderBottom: "none",
-                backgroundColor:
-                  packageManager === manager ? "lightgray" : "transparent",
-                color: packageManager === manager ? "black" : "white",
-              }}
-              key={manager}
-              onClick={() => setPackageManager(manager)}
-            >
-              {manager}
+    <div className="background">
+      <div className="background-anim">
+        <div className="content-wrapper">
+          <h1 className="header-text">Create Web3 UI</h1>
+          <h3>Web3 Connected in Seconds</h3>
+          <div className="manager-block">
+            <div className="manager">
+              {["bun", "pnpm", "yarn", "npm"].map((manager) => (
+                <div
+                  className={
+                    packageManager === manager
+                      ? "manager-option selected"
+                      : "manager-option"
+                  }
+                  key={manager}
+                  onClick={() => setPackageManager(manager)}
+                >
+                  {manager}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
-      <div style={codeBlockStyle}>
-        <SyntaxHighlighter
-          language="javascript"
-          style={dracula}
-          className="codeblock"
-        >
-          {managerText}
-        </SyntaxHighlighter>
-        <div
-          style={{
-            background: "rgb(40, 42, 54)",
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "end",
-            paddingRight: "20px",
-            cursor: "pointer",
-            flex: 1,
-            color: `${isCopied ? "green" : "lightgray"}`,
-          }}
-          onClick={copyToClipboard}
-        >
-          {isCopied ? <FiCheck size={20} /> : <FiCopy size={20} />}
-        </div>
-      </div>
-      <div
-        className="manager-block"
-        style={{ textAlign: "center", width: "80%", margin: "0 auto" }}
-      >
-        <div
-          style={{
-            display: "inline-block",
-            padding: "10px",
-            cursor: "pointer",
-            border: "1px solid slategray",
-            marginTop: "20px",
-            backgroundColor: "rgb(40, 42, 54)",
-            color: "rgb(248, 248, 242)",
-            justifyContent: "center",
-          }}
-        >
-          Get started at{" "}
-          {packageManager === "bun" ? (
-            <a target="_blank" href="https://bun.sh">
-              https://bun.sh
-            </a>
-          ) : packageManager === "pnpm" ? (
-            <a target="_blank" href="https://pnpm.io">
-              https://pnpm.io
-            </a>
-          ) : packageManager === "yarn" ? (
-            <a
-              target="_blank"
-              href="https://yarnpkg.com/getting-started/install"
+          </div>
+          <div style={codeBlockStyle}>
+            <SyntaxHighlighter
+              language="javascript"
+              style={dracula}
+              className="codeblock"
             >
-              https://yarnpkg.com
-            </a>
-          ) : (
+              {managerText}
+            </SyntaxHighlighter>
+            <div className="clipboard-container" onClick={copyToClipboard}>
+              {isCopied ? <FiCheck size={20} /> : <FiCopy size={20} />}
+            </div>
+          </div>
+          <div className="manager-block">
+            <div className="get-started-link">
+              Get started at{" "}
+              {packageManager === "bun" ? (
+                <a target="_blank" href="https://bun.sh">
+                  https://bun.sh
+                </a>
+              ) : packageManager === "pnpm" ? (
+                <a target="_blank" href="https://pnpm.io">
+                  https://pnpm.io
+                </a>
+              ) : packageManager === "yarn" ? (
+                <a
+                  target="_blank"
+                  href="https://yarnpkg.com/getting-started/install"
+                >
+                  https://yarnpkg.com
+                </a>
+              ) : (
+                <a
+                  target="_blank"
+                  href=" https://docs.npmjs.com/downloading-and-installing-node-js-and-npm"
+                >
+                  https://npmjs.com
+                </a>
+              )}
+            </div>
+          </div>
+          <h3 className="demo">
             <a
+              href="https://create-web3-ui-demo.vercel.app/"
               target="_blank"
-              href=" https://docs.npmjs.com/downloading-and-installing-node-js-and-npm"
+              className="serif"
             >
-              https://npmjs.com
+              Demo
             </a>
-          )}
+          </h3>
+          <h2>What's Included?</h2>
+          <div className="box-container">
+            {boxes.map((box, index) => (
+              <div className="box" key={index}>
+                <h2 className="box-title serif">{box.title}</h2>
+                <div className="box-content">
+                  {box.images.map((image, imageIndex) => (
+                    <div className="box-image-container" key={imageIndex}>
+                      <a href={image.link} target="_blank">
+                        <img
+                          src={image.src}
+                          alt={image.text}
+                          className={
+                            image.src === "/connectwallet.png"
+                              ? "connect-image"
+                              : "box-image"
+                          }
+                        />
+                        <p className="box-image-text">{image.text}</p>
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <h2 style={{ paddingTop: "10px" }}>What's Included?</h2>
-      <div className="tech-stack">
-        <div>React</div>
-      </div>
-      <div className="tech-stack">
-        <div className="tech-stack">Vite</div>
-        <div style={{ color: "#f1f1f1" }}>or</div>
-        <div className="tech-stack">NextJS</div>
-      </div>
-      <div className="tech-stack">
-        <div className="tech-stack">TypeScript</div>
-        <div style={{ color: "#f1f1f1" }}>or</div>
-        <div className="tech-stack">JavaScript</div>
-      </div>
-      <div>
-        <div className="tech-stack">Viem</div>
-        <div className="tech-stack">Wagmi</div>
-        <div className="tech-stack">ConnectKit</div>
-      </div>
-      <div>
-        <div className="tech-stack">Zustand</div>
-        <div className="tech-stack">Tanstack Query</div>
+        <div className="footer">
+          <div className="footer-item">
+            <a
+              href="https://github.com/defispartan"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              built by defispartan
+            </a>
+          </div>
+          <div className="footer-item">
+            <a
+              href="https://github.com/defispartan/create-web3-ui/blob/master/LICENSE"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              MIT license
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
