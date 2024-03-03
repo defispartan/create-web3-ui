6ebdd41454b52f8b29e78676d63a2de038b0faa6
aschmidt20 <andrew@aave.com>
2024-03-02 22:27:06 -0600

    feat: separate types

---
+++ template/src/types.ts
@@ -0,0 +1,23 @@
+import { Chain, Transport } from "viem";
+
+export interface ContractConfig {
+  address: `0x${string}`;
+  abi: any;
+  name: string;
+}
+
+export interface AppProps {
+  walletConnectProjectId: string;
+  appName: string;
+  appDescription: string;
+  appUrl: string;
+  appIcon: string;
+}
+
+export interface Web3Config {
+  availableChains: Chain[];
+  defaultChain: Chain;
+  contracts: Record<number, ContractConfig[]>;
+  appProps: AppProps;
+  transports: Record<number, Transport>;
+}
